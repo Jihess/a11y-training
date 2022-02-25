4cf4c8abda8c271499c2a9cb7f7acef8fc6952bc
Jérémy SCHERRER <jeremy.scherrer@valtech.com>
2022-02-25 12:12:41 +0100

    modal, form, accordion exercices

--- app2.js
+++ app2.js
@@ -40,14 +40,61 @@ form.addEventListener('submit', (event) => {
   }
 });
 
+
+const FOCUS_CANDIDATES = `
+  a, button, input, select, textarea, svg, area, details, summary,
+  iframe, object, embed, 
+  [tabindex], [contenteditable]
+`;
+
+const trapFocus = (focusNode, rootNode = document) => {
+  const nodes = [...rootNode.querySelectorAll(FOCUS_CANDIDATES)]
+    .filter(node => !focusNode.contains(node) && node.getAttribute('tabindex') !== '-1');
+  nodes.forEach(node => node.setAttribute('tabindex', '-1'));
+  return {
+    release() {
+      nodes.forEach(node => node.removeAttribute('tabindex'));
+    },
+  };
+};
+
+const modal = document.querySelector('.modal'),
+openModalBtn = document.getElementById('openModal'),
+closeModalBtn = document.getElementById('closeModal'),
+modalBackdrop = document.querySelector('.modal-backdrop');
+let focusTrap;
+
+openModalBtn.addEventListener('click', () => {
+    modalBackdrop.removeAttribute('hidden');
+    modal.removeAttribute('hidden');
+    closeModalBtn.focus();
+    focusTrap = trapFocus(modal);
+});
+
+closeModalBtn.addEventListener('click', () => {
+    modalBackdrop.setAttribute('hidden', '');
+    modal.setAttribute('hidden', '');
+    focusTrap.release();
+    openModalBtn.focus();
+});
+
 const openPanelsButtons = document.querySelectorAll('.accordion-section .open-panel');
 openPanelsButtons.forEach(btn => {
   btn.addEventListener('click', () => {
     document.querySelectorAll('.accordion-section .panel').forEach(panel => {
+      // hide all panels except the one right after the clicked button
       if (panel.previousElementSibling !== btn) {
         panel.setAttribute('hidden', '');
+        panel.previousElementSibling.setAttribute('aria-expanded', false);
       }
     });
     btn.nextElementSibling.toggleAttribute('hidden');
+
+    // toggle aria-expanded value
+    if (btn.getAttribute('aria-expanded') === 'true') {
+      btn.setAttribute('aria-expanded', false);
+    } else {
+      btn.setAttribute('aria-expanded', true);
+    }
   });
 });
